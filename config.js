5ed11f6007f610b1ca105bd0ac04b28c05ab8750
nhanclshrt <90253299+nhanclshrt@users.noreply.github.com>
2021-12-18 17:44:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,15 @@ const CONFIG = {
     introDesc: `Nhân làm cái này này mục đích  là Nhân hong mún mập mờ nữa, mình mập rõ đi Sen =))`,
     btnIntro: 'Oke ha. Dô nè ❤',
     title: 'Giờ mình mập rõ nha Sen 😘',
-    desc: 'Cái này là giờ Sen chọn nè😊                                                                                                                         Nếu mà Sen cảm thấy nó lãng mạn quá á thì Sen nhấp đồng ý nha, còn mà Sen thấy nó sến súa quá á thì Sen cũng nhấp đồng ý luôn =))',
+    desc: 'Nhân kh hứa là cả đời này iu Sen bên Sen hay gì gì đó nhma Nhân sẽ iu Sen, chân thành với Sen hơn ai hết ❤ 
+      Cái này là giờ Sen chọn nè😊 Nếu mà Sen cảm thấy nó lãng mạn quá á thì Sen nhấp đồng ý nha, còn mà Sen thấy nó sến súa quá á thì Sen cũng nhấp đồng ý luôn =))',
     btnYes: 'Sen đồng ý ❤',
     btnNo: 'Không đồng ý nha 🥺🥺 ',
-    question: 'Sen là 1 cô gái xinh đẹp, cũng có rất nhiều sự lựa chọn, sao Sen chọn Nhân z',
+    question: 'Sen là 1 cô gái xinh đẹp, cũng có rất nhiều sự lựa chọn, sao Sen chọn Nhân z =))',
     btnReply: 'Gửi cho vũ trụ giữ dùm 2d con <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Yup Nhân biết mà, iuuuu Sen nhìu, iuuu Sen 3000, 10000 luonn',
-    messDesc: 'Nhân kh hứa là cả đời này iu Sen bên Sen hay gì gì đó nhma Nhân sẽ iu Sen, chân thành với Sen hơn ai hết (hong tính mẹ nha) =)))',
+    mess: 'Yup Nhân biết mà, iuuuu Sen nhìu, iuuu Sen 3000, 10000 luonn ❤',
+    messDesc: 'Nhân kh hứa là cả đời này iu Sen bên Sen hay gì gì đó nhma Nhân sẽ iu Sen, chân thành với Sen hơn ai hết ❤',
     btnAccept: 'Kêla 💋',
     messLink: 'https://m.me/nhan.clshrt' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
